5eac6a2e535328de3f09a1b6032e3a55701b4b0b
Azman234 <49059049+Azman234@users.noreply.github.com>
2020-03-24 00:08:18 +1300

    Create PipeNode.java

---
+++ LGE/src/PipeNode.java
@@ -0,0 +1,35 @@
+
+public class PipeNode extends BoardNode
+{
+	private boolean isPipeMouth;
+	private boolean isPipeExit;
+	private PipeNode pipePartner;
+	
+	//constructoers
+	PipeNode(int nodeNumber) 
+	{
+		super(nodeNumber);
+		super.setFinish(false);
+		super.setStart(false);
+	}
+	
+	//getters and seters
+	public boolean isPipeMouth() {
+		return isPipeMouth;
+	}
+	public void setPipeMouth(boolean isPipeMouth) {
+		this.isPipeMouth = isPipeMouth;
+	}
+	public boolean isPipeExit() {
+		return isPipeExit;
+	}
+	public void setPipeExit(boolean isPipeExit) {
+		this.isPipeExit = isPipeExit;
+	}
+	public PipeNode getPipePartner() {
+		return pipePartner;
+	}
+	public void setPipePartner(PipeNode pipePartner) {
+		this.pipePartner = pipePartner;
+	}
+}
